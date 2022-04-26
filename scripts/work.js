365fe25706788066836478cdf970b3b6f521d3e3
ishar <isharjain19@gmail.com>
2022-04-26 15:00:57 +0530

    added work script

---
+++ scripts/work.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('#btn')
+const forms = document.querySelector('#form')
+const linkArray = {}
+function btnClick() {
+  let confirmInfo = confirm(
+    'Are you sure about the above provided information?, after clicking OK you cannot come back to this page to edit information',
+  )
+  if (confirmInfo) {
+    createLinkArray()
+  }
+}
+function createLinkArray() {
+//   const identifier = self.crypto.randomUUID()
+//   // localStorage.clear()
+//   console.log(identifier)
+    for (let i = 0; i < forms.length; i++) {   //creating an array of links provied and storing it into local storage
+        if (forms[i].value) {
+            linkArray[forms[i].id] = forms[i].value;
+        }
+    }
+    localStorage.setItem('links', JSON.stringify(linkArray))
+    console.log(linkArray)
+//   localStorage.setItem('uniqueID', identifier)
+//   const qridentifier = identifier
+//   fetch(
+//     `https://api.qrserver.com/v1/create-qr-code/?data=https://ishar19.github.io/Code-your-own/fetch.html?id=${identifier}`,
+//   )
+//     .then(function (response) {
+//       const data = response.url
+//       localStorage.setItem('url', data)
+//       console.log(data)
+//     })
+//     .then(function (response) {
+//       window.location.replace('./show.html')
+//     })
+}
